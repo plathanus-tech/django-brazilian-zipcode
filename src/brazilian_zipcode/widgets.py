97a3fb6ac452ff7bae8924d0eb4a21812e1e8e51
Leandro de Souza <leandrodesouzadev@gmail.com>
2022-03-02 15:52:38 -0300

    Fix (formatting): Runs black on widget

--- src/brazilian_zipcode/widgets.py
+++ src/brazilian_zipcode/widgets.py
@@ -3,11 +3,9 @@ from django.utils.safestring import mark_safe
 
 
 class AutoBrazilianZipCodeInput(widgets.TextInput):
-
     def render(self, name, value, **attrs):
-        attrs['meta_id'] = 'meta_zipcode_info'
+        attrs["meta_id"] = "meta_zipcode_info"
         return super().render(name, value, attrs)
-
 
     class Media:
         js = ("getAddressInfo.js",)
